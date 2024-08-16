b33fa78d292545268d190c310598c781661c4260
hacktown <hei.ando.14@gmail.com>
2024-08-16 19:28:50 +0300

    feat: organization service

--- numPy/src/main/java/dev/razafindratelo/numPy/services/organization/oraganizationImpl/OraganizationServiceImpl.java
+++ numPy/src/main/java/dev/razafindratelo/numPy/services/organization/oraganizationImpl/OraganizationServiceImpl.java
@@ -1,17 +1,30 @@
 package dev.razafindratelo.numPy.services.organization.oraganizationImpl;
 
+import dev.razafindratelo.numPy.Exceptions.ResourceDuplicatedException;
+import dev.razafindratelo.numPy.dtos.userDtos.IndividualDto;
 import dev.razafindratelo.numPy.dtos.userDtos.OrganizationDto;
+import dev.razafindratelo.numPy.entity.user.Individual;
+import dev.razafindratelo.numPy.entity.user.Organization;
+import dev.razafindratelo.numPy.mapper.userMapper.individualMapper.IndividualMapper;
+import dev.razafindratelo.numPy.mapper.userMapper.organizationMapper.OrganizationMapper;
 import dev.razafindratelo.numPy.repositories.userRepository.OrganizationRepository;
+import dev.razafindratelo.numPy.repositories.userRepository.UserRepository;
 import dev.razafindratelo.numPy.services.organization.OrganizationService;
 
 import java.util.List;
 
 public class OraganizationServiceImpl implements OrganizationService {
 
-    private OrganizationRepository organizationRepository;
+    private UserRepository organizationRepository;
 
     @Override
     public OrganizationDto createOrganization(OrganizationDto organizationDto) {
+        if(!checkOrganizationUnique(organizationDto)){
+            throw new ResourceDuplicatedException("this organization is already exist");
+        }
+        Organization organization = organizationRepository.save(OrganizationMapper.toOrganization(organizationDto));
+
+        return OrganizationMapper.toOrganizationDto(organization);
         return null;
     }
 
@@ -33,5 +46,15 @@ public class OraganizationServiceImpl implements OrganizationService {
     @Override
     public void deleteEventById(String organizationId) {
 
+    }
+
+    public boolean checkOrganizationUnique(OrganizationDto organizationDto) {
+        List<Organization> organizations = organizationRepository.findAll();
+        for(Organization i : organizations) {
+            if (i.getEmail().equals(organizationDto.getEmail())) {
+                return false;
+            }
+        }
+        return true;
     }
 }
