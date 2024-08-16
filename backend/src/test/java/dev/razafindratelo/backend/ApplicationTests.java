63751760fff445cae78b97da3d2fa4eb8dc90c20
Abega1642 <hei.abega.2@gmail.com>
2024-08-16 10:01:12 +0300

    feat: add spring-boot application

---
+++ backend/src/test/java/dev/razafindratelo/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.razafindratelo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
