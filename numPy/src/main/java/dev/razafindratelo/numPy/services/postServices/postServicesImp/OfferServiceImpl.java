6ba82ed4f9887d71d0443e1cad2713bc03d2530c
hacktown <hei.ando.14@gmail.com>
2024-08-16 20:33:44 +0300

    feat:  offer

--- numPy/src/main/java/dev/razafindratelo/numPy/services/postServices/postServicesImp/OfferServiceImpl.java
+++ numPy/src/main/java/dev/razafindratelo/numPy/services/postServices/postServicesImp/OfferServiceImpl.java
@@ -1,37 +1,66 @@
 package dev.razafindratelo.numPy.services.postServices.postServicesImp;
 
+import dev.razafindratelo.numPy.Exceptions.ResourceNotFoundException;
 import dev.razafindratelo.numPy.dtos.postDtos.OfferDto;
+import dev.razafindratelo.numPy.entity.post.Offer;
+import dev.razafindratelo.numPy.entity.user.Individual;
+import dev.razafindratelo.numPy.mapper.postMapper.OfferMapper;
+import dev.razafindratelo.numPy.mapper.userMapper.individualMapper.IndividualMapper;
+import dev.razafindratelo.numPy.repositories.postRepository.PostRepository;
+import dev.razafindratelo.numPy.repositories.userRepository.UserRepository;
 import dev.razafindratelo.numPy.services.postServices.OfferService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @AllArgsConstructor
 @Service
 public class OfferServiceImpl implements OfferService {
+
+    PostRepository offerRepository;
+
     @Override
     public OfferDto createOffer(OfferDto offerDto) {
-        return null;
+        Offer offer = OfferMapper.mapToPost(offerDto);
+        offerRepository.save(offer);
+        return OfferMapper.mapToPostDto(offer);
     }
 
     @Override
     public OfferDto updateOffer(String offerId, OfferDto offerDto) {
-        return null;
+        Offer offer = (Offer) offerRepository.findById(offerId)
+                .orElseThrow(()->
+                        new ResourceNotFoundException("Individual with id " + offerId + " not found")
+                );
+        offer.setStatus(offerDto.getStatus());
+        offer.setTitle(offerDto.getTitle());
+        offer.setDescription(offerDto.getDescription());
+
+        Offer updateOffer = offerRepository.save(offer);
+        return OfferMapper.mapToPostDto(updateOffer);
+
     }
 
     @Override
     public OfferDto getOfferById(String id) {
-        return null;
+        Offer offer =(Offer) offerRepository.findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("Individual with id " + id + " not found"));
+        return OfferMapper.mapToPostDto(offer);
     }
 
     @Override
     public List<OfferDto> getOffers() {
-        return List.of();
+        List<Offer> offers = Collections.singletonList((Offer) offerRepository.findAll());
+
+        return offers.stream()
+                .map(OfferMapper::mapToPostDto)
+                .toList();
     }
 
     @Override
     public void deleteOffer(String id) {
-
+        offerRepository.deleteById(id);
     }
 }
